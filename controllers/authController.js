5c0c9062a961617947a8fd49ab536d8298b1a16d
Shahiduzzaman Bhuiyan <bhuiyanshaheduzzaman@gmail.com>
2025-08-29 00:45:12 +0600

    login fixing

--- controllers/authController.js
+++ controllers/authController.js
@@ -113,8 +113,8 @@ export const loginUser = async (req, res) => {
       },
     });
   } catch (err) {
-    console.error("Login Error:", err);
-    res.status(500).json({ message: "Server error" });
+    console.error("Login Error:", err.stack || err);
+    res.status(500).json({ message: err.message || "Server error" });
   }
 };
 
