0ea23da77845df890e94c5e4067df0d981bcae40
Md. Shahiduzzaman Bhuiyan <bhuiyanshaheduzzaman@gmail.com>
2025-09-03 14:24:17 +0600

    get Item added

--- routes/item.js
+++ routes/item.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
+const item = require('../models/Item')
 const auth = require("../middleware/auth");
 const {
   addItem,
